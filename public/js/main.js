19decffad512077cdf0aea844fccaffd018ed255
Basile Malinine <D:\Basile\The Bat!>
2023-08-09 08:52:33 +0300

    Доработан скрипт public/js/main.js: уточнение расположения Населённого пункта, добавлены комментарии к коду; правка index.html, теперь не "Город:", а "Населённый пункт:".

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,7 @@
 const token = '3a202d94848db5bf4d7e51ce26885ad0c9440860';
 
 $(document).ready(() => {
+    // Подключаем к полю Адрес формы реакцию на выбор подсказки
     $('#i-address').suggestions({
         token: token,
         type: 'ADDRESS',
@@ -8,18 +9,40 @@ $(document).ready(() => {
     });
 });
 
+// Ф-ция разбирает полученную подсказку и заполняет поля формы
 function parseData(sgs) {
+    // Получаем объект data из выбранной подсказки
     const d = sgs.data;
+
+    // Извлекаем все необходимые поля
+    let region = d.region_with_type;
     let city = d.city_with_type
+    let settlement = d.settlement_with_type;
     let street = d.street_with_type;
-    let house = `${d.house_type} ${d.house}`;
-    let block = `${d.block_type} ${d.block}`;
+    let house = '';
+    if (d.house) house = `${d.house_type} ${d.house}`;
+    let block = '';
+    if (d.block) block = `${d.block_type} ${d.block}`;
+
+    // Если Город расположен в Области, добавляем в начало Область
+    let settlementFull = '';
+    if (region !== city)
+        settlementFull = `${region}, ${city}`;
+    else
+        settlementFull = city;
+
+    // Если это Населённый пункт, добавляем
+    if (settlement !== null)
+        settlementFull += `, ${settlement}`;
 
     let houseBlock = house;
+
+    // Если в адресе у Дома указан Корпус, добавляем
     if (d.block !== null)
         houseBlock += `, ${block}`;
 
-    $('#i-city').val(city);
+    // Заполняем поля в форме
+    $('#i-city').val(settlementFull);
     $('#i-street').val(street);
     $('#i-house').val(houseBlock);
 }
